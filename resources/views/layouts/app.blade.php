7da39eb62df31ff0e3c2fbed9d8997ef25f855a4
ariwira <prasetyowira@gmail.com>
2016-09-24 11:50:36 +0700

    add partials layout add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
     <div class="container">
         @yield('content')
     </div>
+@include('partials.footer')
 <!-- JavaScripts -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7">
+                <img src="storage/img/logo.png">
+                <p>Bimbel USM STAN</p>
+                <p></p>
+                <p>Jl. Bintaro city no 12 tanggerang</p>
+            </div>
+            <div class="col-sm-5">
+                <p>Contact us and we'll get back to you within 24 hours.</p>
+                <p><span class="glyphicon glyphicon-map-marker"></span> Jakarta, Indonesia</p>
+                <p><span class="glyphicon glyphicon-phone"></span> +62 819992744</p>
+                <p><span class="glyphicon glyphicon-envelope"></span> supremen@stan.com</p>
+            </div>
+        </div>
+    </div>
+</footer>
