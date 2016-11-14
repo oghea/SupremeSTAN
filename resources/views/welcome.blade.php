b9e3b504acb4e481ed9edbda9833d0e935dc7e07
ariwira <prasetyowira@gmail.com>
2016-11-14 20:34:26 +0700

    tes-1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SupremeSTAN Under Development!</title>
+    <title>SupremeSTAN</title>
 
     <!-- Bootstrap Core CSS -->
 
